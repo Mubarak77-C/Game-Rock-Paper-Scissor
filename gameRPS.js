05f97dda79cffb307def12ef4c7c62f83b74c504
Mubarak77-C <mubaraksache77@gmail.com>
2026-09-20 17:21:42 +0530

    "from PR  solved issue of data type entering anything"

--- gameRPS.js
+++ gameRPS.js
@@ -38,19 +38,22 @@ function playground(userChoice, computerChoice) {
     // console.log("User choice is ", userChoice);
     // console.log("computer box hold from machine random ", computerChoice);
 
+
+
+
     //now with conditon game with rule draw or win or loose
 
-    if(computerChoice===userChoice) {
+    if (computerChoice === userChoice) {
         console.log(`Both User and computer are draw`)
     }
     //here i will check all possiblity of winning of user choice
-    else if(
-        (userChoice==="rock" && computerChoice === "scissor") ||
-        (userChoice==="scissor" && computerChoice === "paper") ||
-        (userChoice==="paper" && computerChoice === "rock") 
-    ){
+    else if (
+        (userChoice === "rock" && computerChoice === "scissor") ||
+        (userChoice === "scissor" && computerChoice === "paper") ||
+        (userChoice === "paper" && computerChoice === "rock")
+    ) {
         console.log(`User WIN!`)
-    }else{
+    } else {
         console.log(`Computer WIN! : User better luck next time!`)
     }
 
@@ -60,7 +63,26 @@ function playground(userChoice, computerChoice) {
     console.log(`User choice is : ${userChoice}`);
     console.log(`computer box hold from machine random : ${computerChoice}`);
 }
-let userInput = prompt("Enter Rock,Paper,Scissor").toLowerCase();
+
+// let userInput = prompt("Enter Rock,Paper,Scissor").toLowerCase();
+//herer i got PR
+//now i got pull request that input prompt any data type added it taking so it should take data type as what array have not anything like number etc apart from array values string
+//initialise input empty var to start loop to check
+let userInput = "";
+// The Trap Condition: Keep looping AS LONG AS the choices array does NOT include what the user typed
+while (!choices.includes(userInput)) {  //if not arr value include by user input
+    let rawPrompt = prompt("Choose your weapon! Enter rock, scissor, or paper:");
+    // Safety Net: If the user clicks 'Cancel', rawPrompt is null. We turn it into an empty string to prevent crashes.
+    if (rawPrompt === null) {
+        userInput = "";
+    } else {
+        userInput = rawPrompt.toLowerCase().trim();
+    }
+
+}
+
+
+
 let computerInput = getComputerChoice(); //from function call dirext and down with argument pass to paramater it like empty box placeholder type where he get this argument pass but it will run two time one before promt and later promt and random choice computer will differ change like first befor it will choose index 0 then later may chose 1 
 //so above wher is calling comment it simple line no 29
 playground(userInput, computerInput);
