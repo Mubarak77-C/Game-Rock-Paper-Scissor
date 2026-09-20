c6e81fa6bebbc99d2e1dd4c48f6aa12f0d7db171
Mubarak77-C <mubaraksache77@gmail.com>
2026-09-20 14:49:47 +0530

    "initialise in array and checked array for of loop to fetch and function call "

--- gameRPS.js
+++ gameRPS.js
@@ -2,3 +2,18 @@ console.log('hi');
 
 let msg = "Game start"
 document.getElementById("status").innerHTML= msg; 
+
+//game begin 
+//intiailise in array 
+const choices = ["rock","scissor","paper"];
+
+//to check whether from array we are fetched all value from index 0 
+// for(choice of choices){
+//     console.log(choice);  //done display all value rock scissor paper;
+// }  
+
+function getComputerChoice(){
+console.log("function call");   //working 
+}
+
+getComputerChoice();  //calling
