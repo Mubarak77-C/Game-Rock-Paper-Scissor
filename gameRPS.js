c23f36644531d7b35c279c32cdcbfefac399a815
Mubarak77-C <mubaraksache77@gmail.com>
2026-09-20 15:47:53 +0530

    "did mistake logic if  else if and else in > and < condtion it compare with string and all game rule will break r come after p alpha index rather else if with three condtion && and || for win , fix it"

--- gameRPS.js
+++ gameRPS.js
@@ -26,19 +26,37 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-getComputerChoice();  //calling
+//calling function to check where two time run so comment to not call 
+//where it calling from line 45
 
+//getComputerChoice();  
 
 //here confuse where to take input from user where i know computer input coming from above getComputerChoice
 //So for input user take prompt argument to pass in parameter ie empty box like placeholder paramater
 function playground(userChoice, computerChoice) {
-
+    //this is simple to call with string where below backstick is good to call
     // console.log("User choice is ", userChoice);
     // console.log("computer box hold from machine random ", computerChoice);
 
+    //now with conditon game with rule draw or win or loose
+
+    if(computerChoice===userChoice) {
+        console.log(`Both User and computer are draw`)
+    }
+    else if(computerChoice>userChoice){
+        console.log(`ComputerUSer  WIN`)
+    }else{
+        console.log(`ComputerUSer LOOSE`)
+    }
+
+
+
+    //with backstick it good for multiple line to print 
     console.log(`User choice is : ${userChoice}`);
     console.log(`computer box hold from machine random : ${computerChoice}`);
 }
 let userInput = prompt("Enter Rock,Paper,Scissor").toLowerCase();
 let computerInput = getComputerChoice(); //from function call dirext and down with argument pass to paramater it like empty box placeholder type where he get this argument pass but it will run two time one before promt and later promt and random choice computer will differ change like first befor it will choose index 0 then later may chose 1 
+//so above wher is calling comment it simple line no 29
 playground(userInput, computerInput);
+
